f596744aadc8b7facf2e50cacde4bc6d1cc76445
vaibhavkothari33 <vaibhavkothari50@gmail.com>
2025-01-08 16:17:04 +0530

    added models,routes,controllers

--- src/models/video.models.js
+++ src/models/video.models.js
@@ -1,5 +1,38 @@
 import mongoose from "mongoose";
 
-const videoSchema = new mongoose.Schema({},{timestamps:true})
+const videoSchema = new mongoose.Schema({
+    videoFile:{
+        type:String,
+        required:true
+    },
+    thumbnail:{
+        type:String,
+        required:true,
+    },
+    title:{
+        type:String,
+        required:true,
+    },
+    description:{
+        type:String,
+        required:true,
+    },
+    duration:{
+        type:Number,
+        required:true,
+    },
+    views:{
+        type:String,
+        required:true,
+    },
+    isPublished:{
+        type:Boolean,
+        default:true,
+    },
+    owner:{
+        type:Schema.Types.ObjectId,
+        ref:"User",
+    }
+},{timestamps:true})
 
 export const video = mongoose.model("Video",videoSchema)
