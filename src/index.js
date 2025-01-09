c76a7afdf693be73343a97f3e49c6c7169218164
vaibhavkothari33 <vaibhavkothari50@gmail.com>
2025-01-10 01:17:08 +0530

    cors and cookie-parser

--- src/index.js
+++ src/index.js
@@ -7,6 +7,18 @@ dotenv.config({
     path: "./env"
 })
 connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000, ()=>{
+        console.log(`Server is running at http://localhost${process.env.PORT}`);
+    })
+    // app.on("error",(error)=>{
+    //     console.log("Error",error);
+    //     throw error;
+    // })
+})
+.catch((error)=>{
+    console.log("Mongo DB connection failed !!! check for the error",error);
+})
 
 
 
