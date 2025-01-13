a986de369aead4de348355c2b1029480052a8078
vaibhavkothari33 <vaibhavkothari50@gmail.com>
2025-01-14 03:19:31 +0530

    utils code

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ app.use(cors({
 }))
 
 app.use(express.json({limit:"16kb"}))
-app.use(express.urlencoded({extended:true,limit:"16kb"}))
+app.use(express.urlencoded({extended:true,limit:"16kb"})) //url en-codeer
 app.use(express.static("public"))
 
 app.use(cookieParser())
