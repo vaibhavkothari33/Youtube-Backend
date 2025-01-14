3a059245fdd786dee9c0c6c52cb7752ca99415da
vaibhavkothari33 <vaibhavkothari50@gmail.com>
2025-01-14 14:53:32 +0530

    aggreation pipe line

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -1,46 +1,84 @@
 import mongoose from "mongoose";
+import jwt from "jsonwebtoken"
+import brcyt from "bcrypt"
 
 const userSchema = new mongoose.Schema({
-    usename:{
-        type:String,
-        required:true,
-        unique:true,
-        lowercase:true,
-        trim:true,
-        index:true,
+    usename: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true,
+        index: true,
     },
-    email:{
-        type:String,
-        required:true,
-        lowercase:true,
-        trim:true,
-        unique:true,
+    email: {
+        type: String,
+        required: true,
+        lowercase: true,
+        trim: true,
+        unique: true,
     },
-    fullName:{
-        type:String,
-        required:true,
-        trim:true,
-        index:true,
+    fullName: {
+        type: String,
+        required: true,
+        trim: true,
+        index: true,
     },
-    avatar:{
-        type:String,
-        required:true,
+    avatar: {
+        type: String,
+        required: true,
     },
-    coverImage:{
-        type:String,
+    coverImage: {
+        type: String,
     },
-    watchHistory:[{
-        type:Schema.Types.ObjectId,
-        ref:"Video"
+    watchHistory: [{
+        type: Schema.Types.ObjectId,
+        ref: "Video"
     }],
-    password:{
-        type:String,
-        require:[true,"Password is required"]
+    password: {
+        type: String, // enquiction is required 
+        require: [true, "Password is required"]
     },
-    refreshToken:{
-        type:String,
+    refreshToken: {
+        type: String,
     },
 
-},{timestamps:true})
+}, { timestamps: true })
 
-export const User = mongoose.model("User",userSchema);
+userSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) return next();
+
+    this.password = brcyt.hash(this.password, 10)
+    next()
+}) // data save kar ne se phele
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+    return await brcyt.compare(password, this.password)
+}
+userSchema.methods.generateAccessToken = function () {
+    return jwt.sign({
+        _id: this._id,
+        email: this.email,
+        username: this.username,
+        fullName: this.fullName,
+    },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+userSchema.methods.generateRefreshToken = function () {
+    return jwt.sign({
+        _id: this._id,
+        email: this.email,
+        username: this.username,
+        fullName: this.fullName,
+    },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_REFRESH_EXPIRY
+        }
+    )
+}
+export const User = mongoose.model("User", userSchema);
--- src/models/video.models.js
+++ src/models/video.models.js
@@ -1,38 +1,41 @@
 import mongoose from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2"
 
 const videoSchema = new mongoose.Schema({
-    videoFile:{
-        type:String,
-        required:true
+    videoFile: {
+        type: String,
+        required: true
     },
-    thumbnail:{
-        type:String,
-        required:true,
+    thumbnail: {
+        type: String,
+        required: true,
     },
-    title:{
-        type:String,
-        required:true,
+    title: {
+        type: String,
+        required: true,
     },
-    description:{
-        type:String,
-        required:true,
+    description: {
+        type: String,
+        required: true,
     },
-    duration:{
-        type:Number,
-        required:true,
+    duration: {
+        type: Number,
+        required: true,
     },
-    views:{
-        type:String,
-        required:true,
+    views: {
+        type: Number,
+        default:0,
     },
-    isPublished:{
-        type:Boolean,
-        default:true,
+    isPublished: {
+        type: Boolean,
+        default: true,
     },
-    owner:[{
-        type:Schema.Types.ObjectId,
-        ref:"User",
+    owner: [{
+        type: Schema.Types.ObjectId,
+        ref: "User",
     }]
-},{timestamps:true})
+}, { timestamps: true })
 
-export const video = mongoose.model("Video",videoSchema)
+videoSchema.plugin(mongooseAggregatePaginate)
+
+export const video = mongoose.model("Video", videoSchema)
