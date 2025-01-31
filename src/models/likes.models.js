c8e52ae7b47a976d77a651f12bbfa97a3edf9e90
vaibhavkothari33 <vaibhavkothari50@gmail.com>
2025-01-31 17:06:53 +0530

    more updates

---
+++ src/models/likes.models.js
@@ -0,0 +1,15 @@
+import mongoose ,{Schema} from "mongoose"
+
+const likeSchema = new Schema({
+    video:{
+    type:Schema.Types.ObjectId,
+    ref:"Video",
+    },
+    comment:{
+        type:Schema.Types.ObjectId,
+        ref:"Comment",
+    },
+    
+},{timestamps:true})
+
+export const Like = mongoose.model("Like",likeSchema);
